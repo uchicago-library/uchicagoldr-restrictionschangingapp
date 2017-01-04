f5d7798af423df7453534a2d56d066ba90745a33
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-01-04 15:38:55 -0600

    first commit

---
+++ restrictionschangingapp/README.md
@@ -0,0 +1,3 @@
+# About
+
+This is a web application to allow archivists to change the restrictions for a particular object
---
+++ restrictionschangingapp/endpoints/routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, render_template
+
+APP_ROUTES = Blueprint('ldrrestrictionschanging', __name__, template_folder='templates')
+
+@APP_ROUTES.route("/", methods=["GET", "POST"])
+def select_an_object():
+    """fill in please
+    """
+    return render_template("front.html")
+
+
+@APP_ROUTES.route("/change/<str:objid>", methods=["GET", "POST"])
+def make_a_change(objid):
+    """fill in please
+    """
+    if request.method == "POST":
+        # get record, load it into pypremis.lib.PremisRecord, get restrictions already present
+        # iterate through current restrictions, flip active to False
+        # create new restriction node; set active to True
+        # append new restriction to restriction list
+        # load new restriciton list with new restriction into PremisRecord
+        # write premis record to path on-disk
+        # return receipt page informing user that restriction has been changed
+        return "not implemented yet"
+    else:
+        return render_template("changeform.hhtml", objectToChange=objid)
