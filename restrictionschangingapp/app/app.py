3fc8388df85a8bf7a6ec67e1bac8a354190a694a
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-01-05 16:23:55 -0600

    make_changes now writes the new rights to record

--- restrictionschangingapp/app/app.py
+++ restrictionschangingapp/app/app.py
@@ -1,5 +1,31 @@
+from configparser import ConfigParser
 from flask import Flask
+
 from ..endpoints.routes import BP
 
+def retrieve_resource_string(resource_path, pkg_name=None):
+    """
+    retrieves the string contents of some package resource
+    __Args__
+    1. resource_path (str): The path to the resource in the package
+    __KWArgs__
+    * pkg_name (str): The name of a package. Defaults to the project name
+    __Returns__
+    * (str): the resource contents
+    """
+    from pkg_resources import Requirement, resource_string
+    if pkg_name is None:
+        pkg_name = __name__.split('.')[0]
+    return resource_string(Requirement.parse(pkg_name), resource_path)
+
+CONFIG_STR = retrieve_resource_string("config/config.ini").decode("utf-8")
+PARSER = ConfigParser()
+PARSER.read_string(CONFIG_STR)
+
 APP = Flask(__name__)
+
+for x in PARSER["CONFIG"]:
+    v = PARSER["CONFIG"][x]
+    APP.config[x.upper()] = v.split(',')
+
 APP.register_blueprint(BP)
--- restrictionschangingapp/endpoints/routes.py
+++ restrictionschangingapp/endpoints/routes.py
@@ -1,13 +1,15 @@
 
 from flask import Blueprint, redirect, request, render_template
-
+from os.path import exists, join
 from pypairtree.utils import identifier_to_path
 from pypremis.lib import PremisRecord
-from pypremis.nodes import RightsExtension
+from pypremis.nodes import Rights, RightsExtension
 from uchicagoldrtoolsuite.bit_level.lib.misc.premisextensionnodes import Restriction,\
     RestrictedObjectIdentifier, RightsExtensionIdentifier
 
 BP = Blueprint('ldrrestrictionschanging', __name__, template_folder='templates')
+
+#identifier = "rqrh1jcd8r4w1/fdfc7dec773811e6b4d10025904dfbb0"
 
 @BP.route("/", methods=["GET", "POST"])
 def select_an_object():
@@ -26,7 +28,25 @@ def select_an_object():
 def make_a_change(accessionid, objid):
     """fill in please
     """
-    current_restriction = "R-S"
+    from flask import current_app
+    root_path = current_app.config.get("LIVEPREMIS_PATH")
+    accession_path = str(identifier_to_path(accessionid))
+    objid_path = str(identifier_to_path(objid))
+    path_to = join(root_path[0], accession_path,
+                   "arf/pairtree_root", objid_path, "arf", "premis.xml")
+    current_restrictions = []
+    if exists(path_to):
+        precord = PremisRecord(frompath=path_to)
+        for right in precord.get_rights_list():
+            extensions = right.get_rightsExtension()
+            for extension in extensions:
+                restriction = extension.get_field("restriction")
+                code = restriction[0].get_field("restrictionCode")
+                active = restriction[0].get_field("active")
+                print((code, active))
+                if active[0] == "True":
+                    current_restrictions.append(code[0])
+    current_restriction = ','.join(current_restrictions)
     current_accession = accessionid
     current_object = "{}/{}".format(current_accession, objid)
     if request.method == "POST":
@@ -37,10 +57,13 @@ def make_a_change(accessionid, objid):
         rights_ext_id = RightsExtensionIdentifier("DOI", "foo")
         restricted_object = RestrictedObjectIdentifier("DOI", current_object)
         restrict = Restriction(new_restriction, True, restricted_object)
+        restrict.set_field("restrictionComment", restriction_comment.strip())
         rights_extension = RightsExtension()
         rights_extension.set_field("rightsExtensionIdentifier", rights_ext_id)
         rights_extension.set_field("restriction", restrict)
-        print(rights_extension)
+        new_rights_element = Rights(rightsExtension=rights_extension)
+        precord.add_rights(new_rights_element)
+        precord.write_to_file(path_to)
         return render_template("receipt.html", objectChanged=current_object,
                                restrictionComment=restriction_comment,
                                oldRestriction=last_restriction, newRestriction=new_restriction)
